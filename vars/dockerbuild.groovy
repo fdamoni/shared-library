4983c539b251e0b4d0f44780989b79aebea069e8
fdamoni <77584343+fdamoni@users.noreply.github.com>
2021-09-06 15:33:06 +0200

    Update dockerbuild.groovy

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -6,7 +6,7 @@ def call(String project) {
   sh 'docker build . -t deekshithsn/devops-training:$Docker_tag'
 		   withCredentials([string(credentialsId: 'docker_password', variable: 'docker_password')]) {
 				    
-				  sh 'docker login -u deekshithsn -p $docker_password'
-				  sh 'docker push deekshithsn/devops-training:$Docker_tag'
+				  sh 'docker login -u fdamoni -p Xhevdet1!'
+				  sh 'docker push fdamoni/devops-training:$Docker_tag'
 			}
 }
